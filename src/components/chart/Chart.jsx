5331b6660066bf274111dd29ec09f73c038ac241
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2023-03-18 18:10:35 +0300

    single page added

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,6 +1,6 @@
 import "./chart.scss"
 import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
   { name: "January", Total: 1200 },
@@ -11,11 +11,11 @@ const data = [
   { name: "June", Total: 1700 },
 ];
 
-const Chart = () => {
+const Chart = ({aspect,title}) => {
   return (
     <div className="chart"> 
-    <div className="title">Last 6 Months (Revenue)</div>
-     <ResponsiveContainer width="100%" aspect={2 / 1 }>
+    <div className="title">{title}</div>
+     <ResponsiveContainer width="100%" aspect={aspect }>
         <LineChart
           width={500}
           height={300}
